48e778ab97a4aa8be4762eafaa7a189e9c33ad67
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-08-16 22:32:42 +0530

    Commit from intelliJ ide

--- gohul2.java
+++ gohul2.java
@@ -2,5 +2,6 @@ public class gohul2 {
     public static void main(String[] args) {
         System.out.println("Hello! this is for updating code from downstream main branch");
         // adding comment for git branching
+        // Adding some other comment to fetch
     }
 }
